e339bc0e7e2b3020c8aba4d4e29a3340d6cf4090
janjamanja <nikola.aleksa.janjic@gmail.com>
2024-03-29 12:43:41 +0100

    commenting

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ const deleteTask = taskElement => {             //used as part of the process of
     saveTasks();
 }
 
-const loadTasks = () => {
-    const savedTasks = localStorage.getItem('tasks');
+const loadTasks = () => {   //ran at the very start in order to load previosly saved tasks
+    const savedTasks = localStorage.getItem('tasks');       
 
     if (savedTasks) {
         tasks.innerHTML = savedTasks;
     }
 }
 
-const saveTasks = () => {
+const saveTasks = () => {   //saves tasks to localStorage, so they can be accessed even when the website is closed
     localStorage.setItem('tasks', tasks.innerHTML);
 }
 
-submitBtn.addEventListener('click', () => {
-    const name = document.getElementById('name').value;
-    const deadline = document.getElementById('deadline').value;
-    const category = document.getElementById('category').value;
+submitBtn.addEventListener('click', () => {     //main function for adding tasks to the list
+    const name = document.getElementById('name').value;             
+    const deadline = document.getElementById('deadline').value;     
+    const category = document.getElementById('category').value;         //variables being declared
 
-    if (name === "" || deadline === "") {
+    if (name === "" || deadline === "") {                               
         alert('The name and deadline categories are required!')
     } else if (name.length > 16){
         alert("Name musn't be over 16 characters!")
@@ -37,13 +37,13 @@ submitBtn.addEventListener('click', () => {
         tasks.innerHTML += `
         <div class="task">
             <button class="closeBtn">X</button>
-            <h1>${name}</h1>
-            <p>Deadline: ${deadline}</p>
+            <h1>${name}</h1>                            //this is the structure of a task, it comes with a button to delete it
+            <p>Deadline: ${deadline}</p>        
             <p>${category}</p>
         </div>    
         `
     }
-    saveTasks();
+    saveTasks();            //tasks are saved to the localStorage
 })
 
 tasks.addEventListener('click', event => {                          //delete a singular task by pressing "X" button
